27290742edca1a9b6af827a71d4122ab380166e3
Nadeem Ansari <s524968@mail.nwmissouri.edu>
2017-11-24 17:35:08 -0800

    Add GET/questions/:id route.

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ require('./config/config');
 
 const express = require('express');
 const bodyParser = require('body-parser');
+const { ObjectID } = require('mongodb');
 
 var { mongoose } = require('./db/mongoose');
 var { Question } = require('./models/question');
@@ -13,9 +14,27 @@ app.use(bodyParser.json());
 
 // GET/questions
 app.get('/questions', (req, res) => {
-    Question.find().then((questions) => {
-        res.send(questions);
+    Question.find().then((docs) => {
+        res.send(docs);
     }, (e) => {
+        res.status(400).send(e);
+    });
+});
+
+// GET/questions/:id
+app.get('/questions/:id', (req, res) => {
+    var id = req.params.id;
+
+    if (!ObjectID.isValid(id)) {
+        return res.status(404).send();
+    }
+
+    Question.findById(id).then((doc) => {
+        if (!doc) {
+            return res.status(404).send();
+        }
+        res.send(doc);
+    }).catch((e) => {
         res.status(400).send(e);
     });
 });
@@ -34,7 +53,7 @@ app.post('/questions', (req, res) => {
 });
 
 app.listen(port, () => {
-  console.log(`Started up at port ${port}`);
+    console.log(`Started up at port ${port}`);
 });
 
 module.exports = { app };
