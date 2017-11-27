8f20a2e3650dba2fb51aa1c478ba414f111223c8
Nadeem Ansari <s524968@mail.nwmissouri.edu>
2017-11-26 16:51:23 -0800

    Add CORS.
    Also added seed data file.

--- server/server.js
+++ server/server.js
@@ -12,6 +12,11 @@ var app = express();
 const port = process.env.PORT;
 
 app.use(bodyParser.json());
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 // GET/questions
 app.get('/questions', (req, res) => {
