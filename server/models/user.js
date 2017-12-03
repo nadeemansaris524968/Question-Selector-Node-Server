c95270ba44c6a594ed18cf753268200437790db7
Nadeem Ansari <s524968@mail.nwmissouri.edu>
2017-12-03 12:28:09 -0800

    Update config to use json file.

--- server/models/user.js
+++ server/models/user.js
@@ -49,7 +49,7 @@ UserSchema.methods.generateAuthToken = function () {
     // user is model instance or doc that is about to be saved
     var user = this;
     var access = 'auth';
-    var token = jwt.sign({ _id: user._id.toHexString(), access }, 'abc123').toString();
+    var token = jwt.sign({ _id: user._id.toHexString(), access }, process.env.JWT_SECRET).toString();
 
     user.tokens.push({ access, token });
 
@@ -80,7 +80,7 @@ UserSchema.statics.findByToken = function (token) {
     var decoded;
 
     try {
-        decoded = jwt.verify(token, 'abc123');
+        decoded = jwt.verify(token, process.env.JWT_SECRET);
     } catch (e) {
         // Not a valid token
         return Promise.reject();
