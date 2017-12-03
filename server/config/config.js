041e52fe3e65246e295b435a9040a490a2216278
Nadeem Ansari <s524968@mail.nwmissouri.edu>
2017-12-03 14:14:32 -0800

    Config changes.

--- server/config/config.js
+++ server/config/config.js
@@ -1,6 +1,6 @@
 var env = process.env.NODE_ENV || 'development';
 
-if (env === 'development' || 'test') {
+if (env === 'development' || env === 'test') {
     var config = require('./config.json');
     var envConfig = config[env];
     Object.keys(envConfig).forEach((key) => {
