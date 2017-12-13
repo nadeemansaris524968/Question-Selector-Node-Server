41a810e2569cb458843a56cdc6854227749744df
Nadeem Ansari <s524968@mail.nwmissouri.edu>
2017-12-12 22:43:02 -0800

    Add support to receive x-auth custom header for logout using angular.

--- server/server.js
+++ server/server.js
@@ -17,7 +17,7 @@ app.use(bodyParser.json());
 app.use(function (req, res, next) {
     res.setHeader("Access-Control-Allow-Origin", "*");
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-    res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, x-auth");
     res.setHeader("Access-Control-Expose-Headers", "x-auth");
     next();
 });
