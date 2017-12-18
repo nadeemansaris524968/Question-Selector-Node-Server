8b24081dc142373c660392984de37794c549ab6a
Nadeem Ansari <s524968@mail.nwmissouri.edu>
2017-12-18 07:22:54 -0800

    Send appropriate user login error for login in angular.

--- server/models/user.js
+++ server/models/user.js
@@ -114,9 +114,7 @@ UserSchema.statics.findByCredentials = function (email, password) {
     return User.findOne({ email }).then((user) => {
         // User doesn't exist
         if (!user) {
-            return Promise.reject({
-                errorMessage: 'No user exists'
-            });
+            return Promise.reject('No user exists');
         }
 
         return new Promise((resolve, reject) => {
@@ -124,9 +122,7 @@ UserSchema.statics.findByCredentials = function (email, password) {
                 if (res) {
                     resolve(user);
                 } else {
-                    reject({
-                        errorMessage: 'Incorrect Password'
-                    });
+                    reject('Incorrect Password');
                 }
             });
         });
