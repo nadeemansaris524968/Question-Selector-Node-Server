05e6d32f509aa76aae3b32ce75babc72bf681760
Nadeem Ansari <s524968@mail.nwmissouri.edu>
2017-12-21 16:31:09 -0800

    Add test for GET /questions.

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -41,8 +41,86 @@ var question = {
     ]
 };
 
+var questionsSeed = [
+    {
+        img: "Dummy question1",
+        isAnswered: false,
+        if_thens: [
+            {
+                then_questions: [
+                    {
+                        answers: [
+                            "Yes",
+                            "No",
+                            "Not applicable"
+                        ],
+                        text: "Dummy question"
+                    }
+                ],
+                if_question: {
+                    answers: [
+                        "Yes",
+                        "No",
+                        "Not applicable"
+                    ],
+                    text: "Dummy question"
+                }
+            }
+        ],
+        independent: [
+            {
+                answers: [
+                    "Yes",
+                    "No",
+                    "Not applicable"
+                ],
+                text: "Dummy question"
+            }
+        ]
+    },
+    {
+        img: "Dummy question 2",
+        isAnswered: false,
+        if_thens: [
+            {
+                then_questions: [
+                    {
+                        answers: [
+                            "Yes",
+                            "No",
+                            "Not applicable"
+                        ],
+                        text: "Dummy question"
+                    }
+                ],
+                if_question: {
+                    answers: [
+                        "Yes",
+                        "No",
+                        "Not applicable"
+                    ],
+                    text: "Dummy question"
+                }
+            }
+        ],
+        independent: [
+            {
+                answers: [
+                    "Yes",
+                    "No",
+                    "Not applicable"
+                ],
+                text: "Dummy question"
+            }
+        ]
+    }
+]
+
 beforeEach((done) => {
-    Question.remove({}).then(() => done());
+    Question.remove({}).then(() => {
+        return Question.insertMany(questionsSeed);
+    })
+        .then(() => done());
 });
 
 describe('POST /questions', () => {
@@ -95,11 +173,23 @@ describe('POST /questions', () => {
                     return done(err);
                 }
 
-                Question.find().then((questions) => {
+                Question.find({ img: 'Dummy question' }).then((questions) => {
                     expect(questions.length).toBe(1);
                     expect(questions[0].img).toBe(question.img);
                     done();
                 }).catch((e) => done(e));
             });
+    });
+});
+
+describe('GET /questions', () => {
+    it('should get all todos', (done) => {
+        request(app)
+            .get('/questions')
+            .expect(200)
+            .expect((res) => {
+                expect(res.body.length).toBe(2);
+            })
+            .end(done);
     });
 });
